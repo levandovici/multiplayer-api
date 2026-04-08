5f25f8964212d4a6b15778fd2a86249a1b612d72
Nichita Levandovici <nichitalnc@gmail.com>
2026-04-08 18:03:35 +0300

    fix api - game room

--- api/game_room.php
+++ api/game_room.php
@@ -158,7 +158,7 @@ function createRoom() {
         SELECT mp.matchmaking_id
         FROM matchmaking_players mp
         JOIN matchmaking m ON mp.matchmaking_id = m.matchmaking_id
-        WHERE mp.player_id = ? AND mp.is_online = TRUE AND m.is_started = FALSE
+        WHERE mp.player_id = ? AND mp.is_online = TRUE
         LIMIT 1
     ");
     $stmt->execute([$player['id']]);
